66532a6c3bf7efaf77ef66b2045ed6e4f43ba09a
Tobias Reich <tobias.reich.ich@gmail.com>
2016-07-30 17:07:22 +0200

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const assert = require('chai').assert
+const index  = require('./../src/index')
+
+describe('index()', function() {
+
+	it('should return undefined when called with a function that throws an error', function() {
+
+		assert.strictEqual(index(() => JSON.parse('truee')), undefined)
+
+	})
+
+	it('should return undefined when called without a function', function() {
+
+		assert.strictEqual(index(), undefined)
+
+	})
+
+	it('should return undefined when called with a non-function parameter', function() {
+
+		assert.strictEqual(index(true), undefined)
+
+	})
+
+	it('should return a value when called with a function that returns a value', function() {
+
+		assert.strictEqual(index(() => JSON.parse('true')), true)
+
+	})
+
+})
