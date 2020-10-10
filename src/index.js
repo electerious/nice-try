a80441315e2718d171668cf0f44c23fbc53d3866
Richie Bendall <richiebendall@gmail.com>
2020-10-10 21:59:54 +1300

    Add `.promise()`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,3 +10,14 @@ module.exports = function(fn) {
 	try { return fn() } catch (e) {}
 
 }
+
+/**
+ * Tries to execute an asynchronous function and discards any error that occurs.
+ * @param {Function} fn - Asynchronous function that might or might not throw an error.
+ * @returns {?*} Promise which resolves with the return-value of the asynchronous function when no error occurred.
+ */
+module.exports.promise = async function (fn) {
+
+	try { return await fn() } catch (e) {}
+
+}
