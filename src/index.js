3c2a42bc27219f065e3d3062c6e4170b1ba9854c
Tobias Reich <tobias@electerious.com>
2025-09-11 16:15:50 +0200

    Adjust JSDoc

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * If an error is thrown during execution, the error is silently ignored and `undefined` is returned.
  *
  * @param {Function} fn - The function to execute.
- * @returns {*} The return value of the function, or `undefined` if an error occurred.
+ * @returns {any} The return value of the function, or `undefined` if an error occurred.
  */
 export default function niceTry(fn) {
   try {
